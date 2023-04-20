cb1078e4cc6d131f4620dd30f2d374b0cb652bef
Willem Kempers <willemkempers@gmail.com>
2023-04-20 14:44:25 +0200

    Add build script to Cinder source and BasicApp to make building easier

---
+++ build.sh
@@ -0,0 +1,40 @@
+#!/usr/bin/env bash
+
+function build() {
+    local build_type=$1
+    mkdir -p build
+    (
+        cd build
+        cmake -S .. -B . -GNinja -DCMAKE_EXPORT_COMPILE_COMMANDS=1 -DCMAKE_BUILD_TYPE=$build_type
+        cmake --build .
+    )
+}
+
+function clean() {
+    rm -rf ./build
+}
+
+function main() {
+    local action=${1,,}
+
+    if [[ $action == "" ]]; then
+        build "Debug"
+        [[ $? != 0 ]] && exit
+    elif [[ $action == "debug" ]]; then
+        local build_type=${action^}
+        build $build_type
+        [[ $? != 0 ]] && exit
+    elif [[ $action == "release" ]]; then
+        local build_type=${action^}
+        build $build_type
+        [[ $? != 0 ]] && exit
+    elif [[ $action == "clean" ]]; then
+        clean
+        [[ $? != 0 ]] && exit
+    else
+        echo "usage: $0 [debug|release|clean]"
+        exit 255
+    fi
+}
+
+main $@
